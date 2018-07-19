7c1ebca43fafdbddb6dad5813475369f14c71c6f
Ben Carlisle <bencarlisle15@gmail.com>
2018-07-19 13:05:05 -0600

    Overflow home

--- phpRequests/setRets.php
+++ phpRequests/setRets.php
@@ -20,43 +20,51 @@ echo "End Search\n";
 $ar = $results->toArray();
 echo "Start Photos\n";
 for ($q = 0; $q < sizeof($ar); $q++) {
-	// echo $q . " of " . count($ar) . "\n";
-	$dir = "../images/rets/" . $ar[$q]['MLSNumber'];
+	echo $q . " of " . count($ar) . "\n";
 	// if (file_exists($dir)) {
 	// 	continue;
 	// }
-	$url = 'http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&adminDistrict=' . urlencode($ar[$q]["StateOrProvince"]) . '&postalCode=' . urlencode($ar[$q]["PostalCode"]) . '&addressLine=' . urlencode($ar[$q]["FullStreetNum"]) . '&maxResults=1&key=AqCiNn0TIMZxpKWaZIrRDMR0vPnl6l_5i87yU_abqwGokrBH5kXAl1AZvCfeICgH';
-	$geo = file_get_contents($url);
-	$geo = json_decode($geo, true);
-	if (!$geo['resourceSets'][0]['resources'][0]['point']['coordinates']) {
-		echo 'Geocoding failed\n';
-	} else {
-		$ar[$q]["Latitude"] = $geo['resourceSets'][0]['resources'][0]['point']['coordinates'][0];
-		$ar[$q]["Longitude"] = $geo['resourceSets'][0]['resources'][0]['point']['coordinates'][1];
-	}
+	// $url = 'http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&adminDistrict=' . urlencode($ar[$q]["StateOrProvince"]) . '&postalCode=' . urlencode($ar[$q]["PostalCode"]) . '&addressLine=' . urlencode($ar[$q]["FullStreetNum"]) . '&maxResults=1&key=AqCiNn0TIMZxpKWaZIrRDMR0vPnl6l_5i87yU_abqwGokrBH5kXAl1AZvCfeICgH';
+	// $geo = file_get_contents($url);
+	// $geo = json_decode($geo, true);
+	// if (!$geo['resourceSets'][0]['resources'][0]['point']['coordinates']) {
+	// 	echo 'Geocoding failed\n';
+	// } else {
+	// 	$ar[$q]["Latitude"] = $geo['resourceSets'][0]['resources'][0]['point']['coordinates'][0];
+	// 	$ar[$q]["Longitude"] = $geo['resourceSets'][0]['resources'][0]['point']['coordinates'][1];
+	// }
+	$dir = "../images/rets/" . $ar[$q]['MLSNumber'];
+	// if (!file_exists($dir)) {
+	// 	mkdir($dir);
+	// } else if (iterator_count(new FilesystemIterator($dir, FilesystemIterator::SKIP_DOTS)) - 1 == $ar[$q]['PhotoCount']) {
+	// 	echo "Done\n";
+	// 	continue;
+	// }
+	// echo "Downloading " . $ar[$q]['MLSNumber'] . "\n";
 	$photos = $rets->GetObject("Property", "Photo", $ar[$q]['Matrix_Unique_ID'], "*", 0);
-	if (!file_exists($dir)) {
-		mkdir($dir);
-	}
 	for ($i = 0; $i < count($photos); $i++) {
 		file_put_contents($dir . "/" . $i . ".jpg", $photos[$i]->getContent());
 	}
-	$largePhotos = $rets->GetObject("Property", "LargePhoto", $ar[$q]['Matrix_Unique_ID'], "*", 0);
 	$largeDir = "../images/largeRets/" . $ar[$q]['MLSNumber'];
-	if (!file_exists($largeDir)) {
-		mkdir($largeDir);
-	}
+	// if (!file_exists($largeDir)) {
+	// 	mkdir($largeDir);
+	// } else if (iterator_count(new FilesystemIterator($largeDir, FilesystemIterator::SKIP_DOTS)) - 1 == $ar[$q]['PhotoCount']) {
+	// 	echo "Done\n";
+	// 	continue;
+	// }
+	// echo "Downloading Large " . $ar[$q]['MLSNumber'] . "\n";
+	$largePhotos = $rets->GetObject("Property", "LargePhoto", $ar[$q]['Matrix_Unique_ID'], "*", 0);
 	for ($i = 0; $i < count($largePhotos); $i++) {
 		file_put_contents($largeDir . "/" . $i . ".jpg", $largePhotos[$i]->getContent());
 	}
 }
-$data = json_encode($ar);
-$conn = new mysqli("localhost", getDBUser(), getDBPassword(), getDBName());
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error . "\n");
-}
-$query = "UPDATE RetsData SET json_data='" . $conn->real_escape_string($data) . "'";
-mysqli_query($conn, $query);
+// $data = json_encode($ar);
+// $conn = new mysqli("localhost", getDBUser(), getDBPassword(), getDBName());
+// if ($conn->connect_error) {
+// 	die("Connection failed: " . $conn->connect_error . "\n");
+// }
+// $query = "UPDATE RetsData SET json_data='" . $conn->real_escape_string($data) . "'";
+// mysqli_query($conn, $query);
 $_POST['functionname'] = 'sendEmail';
 $_POST['body'] = "Records Updated";
 include('apiRequests.php');
