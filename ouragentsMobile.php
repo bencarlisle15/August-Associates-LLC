78d758710124d58cc96901457d416e9712d68afe
Ben Carlisle <bencarlisle15@gmail.com>
2018-07-31 16:01:49 -0600

    Updated bios

--- ouragentsMobile.php
+++ ouragentsMobile.php
@@ -38,18 +38,18 @@
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
-						<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Joseph McCarthy"></amp-img>
+						<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Bob Nasiatka"></amp-img>
 					</div>
 					<div class="agentInfo">
 						<h2 class="agentName agentElement">Bob Nasiatka</h2>
 						<p class="agentParagraph agentElement">Being a realtor for eleven years has allowed me to pair my knowledge with my passion. I have bought and sold my own investment properties since 1997, eventually expanding that endeavor into managing other client’s properties as well. The culmination of that hard work was the establishment of my property management company, R&R Realty, in 2005.</br></br>Complementing my years of experience, I work hard to stay on real estate’s cutting edge by using the latest industry technology. This allows me to equip both my buyers and sellers with the most up to date information. As a life-long Rhode Island resident, I also have a unique perspective on current trends in the Southeastern New England markets. Clients choose to work with me for my years of experience, ethics and commitment to service. My business is 90% referral and 10% new business which I proudly believe speaks volumes about my client’s satisfaction.</p>
 						<h3 class="agentEmail agentElement">Email: <a href="mailto:bob@necompass.com">bob@necompass.com</a></h3>
-						<h3 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/robert.nasiatka.98">facebook.com/robert.nasiatka.98</a></h3>
+						<!-- <h3 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/robert.nasiatka.98">facebook.com/robert.nasiatka.98</a></h3> -->
 					</div>
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
-						<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Wendy Grave"></amp-img>
+						<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Rick Delmastro"></amp-img>
 					</div>
 					<div class="agentInfo">
 						<h3 class="agentName agentElement">Rick Delmastro</h3>
@@ -58,19 +58,28 @@
 						<!-- <h4 class="agentWebsite agentElement">Website: <a href="http://www.wendygrave.com">wendygrave.com</a></h4> -->
 					</div>
 				</div>
-			</div>
-			<div class="agentWrapper">
+				<div class="agentWrapper">
+					<div class="agentImageWrapper">
+						<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Kari Hernandez"></amp-img>
+					</div>
+					<div class="agentInfo">
+						<h3 class="agentName agentElement">Kari Hernandez</h3>
+						<p class="agentParagraph agentElement">As a Realtor specializing in residential properties, Kari brings an abundant amount of energy, creativity and dedication to her work. Kari first discovered her passion for real estate while working as a Real Estate Administrator and attending college full time. She studied Business Administration and graduated in 2006 with a Bachelors degree in Business Management from Rhode Island College. Kari later transitioned into the Banking/Financial industry where she gained valuable skills and experience that she uses daily in real estate negotiations, educating and building interpersonal relationships with home buyers and sellers and effectively marketing the team's listings.<br/><br/>Friends and clients describe her as ambitious, detailed oriented yet personable and fun to be around. Kari enjoys artistic projects, exploring local restaurants and likes to spend time with her family during her free time. She is wife of a loving firefighter, mom of three boys and actively participates in her community's baseball little league and is a board member of her neighborhood Parent-Teacher Committee.</p>
+						<h4 class="agentEmail agentElement">Email: <a href="mailto:kari@necompass.com">kari@necompass.com</a></h4>
+						<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/KHernandezRealtor/">facebook.com/KHernandezRealtor</a></h4>
+					</div>
+				</div>
+			<!-- <div class="agentWrapper">
 				<div class="agentImageWrapper">
-					<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Wendy Grave"></amp-img>
+					<amp-img class="agentImage" width="240" height="240" src="https://i.stack.imgur.com/34AD2.jpg" alt="Debora Rotondo"></amp-img>
 				</div>
 				<div class="agentInfo">
-					<h3 class="agentName agentElement">Kari Hernandez</h3>
+					<h3 class="agentName agentElement">Debora Rotondo</h3>
 					<p class="agentParagraph agentElement">As a Realtor specializing in residential properties, Kari brings an abundant amount of energy, creativity and dedication to her work. Kari first discovered her passion for real estate while working as a Real Estate Administrator and attending college full time. She studied Business Administration and graduated in 2006 with a Bachelors degree in Business Management from Rhode Island College. Kari later transitioned into the Banking/Financial industry where she gained valuable skills and experience that she uses daily in real estate negotiations, educating and building interpersonal relationships with home buyers and sellers and effectively marketing the team's listings.<br/><br/>Friends and clients describe her as ambitious, detailed oriented yet personable and fun to be around. Kari enjoys artistic projects, exploring local restaurants and likes to spend time with her family during her free time. She is wife of a loving firefighter, mom of three boys and actively participates in her community's baseball little league and is a board member of her neighborhood Parent-Teacher Committee.</p>
-					<h4 class="agentEmail agentElement">Email: <a href="mailto:kari@necompass.com">kari@necompass.com</a></h4>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:kari@necompass.com">debora@necompass.com</a></h4>
 					<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/KHernandezRealtor/">facebook.com/KHernandezRealtor</a></h4>
 				</div>
-			</div>
-		</div>
+			</div> -->
 		</div>
 		<?php include('bin/footer.html'); ?>
 	</body>
