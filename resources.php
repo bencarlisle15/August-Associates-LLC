98bc68ddec71877fda66ccd251448979ddf58da7
Ben Carlisle <bencarlisle15@gmail.com>
2018-08-03 14:51:57 -0600

    Made resources page

---
+++ resources.php
@@ -0,0 +1,84 @@
+		<?php include('bin/head.php'); ?>
+		<link rel="stylesheet" type="text/css" href="/css/resources.css">
+		<link rel="canonical" href="https://www.augustassociatesllc.net/thankyou" />
+		<title>August Associates LLC - Resources</title>
+		<meta name="description" content="Resources provided for you to make your transitions easier. August Associates, your valued guide in real estate." />
+	</head>
+	<body>
+		<?php include('bin/nav.php'); ?>
+		<div id="resourcesSection" class="section">
+			<h2 id="resourcesSectionTitle">Resources</h2>
+			<div id="movingSection" class="typeSection">
+				<h3 id="movingTitle" class="resourceTitle">Moving</h3>
+				<table id="movingTable" class="resourceTable">
+					<tr class="resourceRow headerRow">
+						<th class="resourceName">Name</th>
+						<th class="resourceWebsite">Website</th>
+						<th class="resourceInfo">Info</th>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+				</table>
+			</div>
+			<div id="appliancesSection" class="typeSection">
+				<h3 id="applianceTitle" class="resourceTitle">Appliances</h3>
+				<table id="applianceTable" class="resourceTable">
+					<tr class="resourceRow headerRow">
+						<th class="resourceName">Name</th>
+						<th class="resourceWebsite">Website</th>
+						<th class="resourceInfo">Info</th>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+					<tr class="resourceRow">
+						<th class="resourceName">Moving Moving Co</th>
+						<td class="resourceWebsite"><a href="https://www.movingmovingco.com">movingmovingco.com</a></td>
+						<td class="resourceInfo">The very best moving company</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<?php include('bin/footer.html'); ?>
+	</body>
+</html>
