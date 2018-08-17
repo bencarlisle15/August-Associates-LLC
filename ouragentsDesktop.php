a331b67919bebbc7acaf12e499fa4d261e0fa069
Ben Carlisle <bencarlisle15@gmail.com>
2018-08-17 15:32:20 -0600

    Updated bios and changed desktop layout

--- ouragentsDesktop.php
+++ ouragentsDesktop.php
@@ -15,12 +15,10 @@
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/joseph.jpg" alt="Joseph McCarthy"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Joseph McCarthy</h3>
-						<p class="agentParagraph agentElement">Joe has been in service & sales businesses for 24 years. In recent years, he has focused his knowledge & skills in the real estate industry. His ability to speak two languages (Spanish) has made it easier to communicate with a vast majority of people. Joe is intuitive and sensitive to everyone's needs for honest, sincere assistance in the sometimes - puzzling, home-buying & marketing quests.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:jmccarthy@necompass.com">jmccarthy@necompass.com</a></h4>
-						<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/profile.php?id=100012283484334">facebook.com/profile.php?id=100012283484334</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Joseph McCarthy</h3>
+					<p class="agentParagraph agentElement">Joseph McCarthy is the Founder and Principal Broker of August Associates LLC, Your Valued Guide In Real Estate. He is dedicated to providing outstanding service and knowledge in real estate with over 20 years of experience working for a corporate concern as a Sales Associate and later a Broker Associate.<br/><br/>He maintains a broad practice by representing a full range of clients from first-time homebuyers to second-time home buyers.<br/><br/>The City of Providence, professional companies, and community developers in the non-profit sector have procured his representation to protect and guide their interest.<br/><br/>In 2000, Joseph sat on a house summit panel and began to speak about the lack of education available to the consumer. His stand to this important issue, led him on a crusade of partnering up with local non-profits and eventually becoming a teacher for home buyer education. He didn't stop there. Joseph became active in his local board, Greater Providence Board of Realtors, and became Secretary on the State Association in 2011. He has spoken out on issues that affect real estate and was instrumental in obtaining the tax adjustment on transfer of property in Providence. That earned him recognitions from his peers and numerous awards and nominations including Realtor of the Year.<br/><br/>Joseph continues to broaden his skills as a real estate professional through education not only being the teacher but the pupil. He quotes, "I find that at teaching, I learn more from others than I do teaching." When asked what do you do for pleasure he responded, "Cooking creations. Food innovations inspired by his day". He adds, "It's all about keeping it natural, simple, clean and tasty." As an old friend would say, "The difference between a great chef and a good chef is a pinch of salt."</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:jmccarthy@necompass.com">jmccarthy@necompass.com</a></h4>
+					<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/profile.php?id=100012283484334">facebook.com/profile.php?id=100012283484334</a></h4>
 				</div>
 			</div>
 			<div id="allAgentsList">
@@ -28,53 +26,43 @@
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/wendy.jpg" alt="Wendy Grave"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Wendy Grave</h3>
-						<p class="agentParagraph agentElement">Experienced Realtor with a demonstrated history of working in the real estate industry. Skilled in Microsoft Excel, Customer Service, Microsoft Word, Data Entry, and Microsoft PowerPoint. Strong real estate professional graduated from University of Rhode.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:wgrave@necompass.com">wgrave@necompass.com</a></h4>
-						<h4 class="agentWebsite agentElement">Website: <a href="http://www.wendygrave.com">wendygrave.com</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Wendy Grave</h3>
+					<p class="agentParagraph agentElement">Wendy is a hands-on dedicated Realtor who enjoys taking people on the journey of finding a home that satisfies their needs. Her background in the financial services industry combined with her 15 years as Investor, gives her a unique perspective in real estate. As a Realtor she is responsive to the needs of her clients and offers them a unique preservative. As a real estate investor, Wendy is in tune with current market conditions based on realistic expectations of the ever changing market. She is happy to dedicate the time necessary when representing clients who are pursuing the largest purchase of their life. Wendy can guide clients to a successful real estate transaction with sincere, honest representation. Her goal is not only to close the deal, it is to attain realistic prices that reflect the current market values of the properties. When not negotiating real estate transactions, she practices the art at home as the mother to three children</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:wgrave@necompass.com">wgrave@necompass.com</a></h4>
+					<h4 class="agentWebsite agentElement">Website: <a href="http://www.wendygrave.com">wendygrave.com</a></h4>
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/bob.jpg" alt="Bob Nasiatka"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Bob Nasiatka</h3>
-						<p class="agentParagraph agentElement">Being a realtor for eleven years has allowed me to pair my knowledge with my passion. I have bought and sold my own investment properties since 1997, eventually expanding that endeavor into managing other client’s properties as well. The culmination of that hard work was the establishment of my property management company, R&R Realty, in 2005.<br/><br/>Complementing my years of experience, I work hard to stay on real estate’s cutting edge by using the latest industry technology. This allows me to equip both my buyers and sellers with the most up to date information. As a life-long Rhode Island resident, I also have a unique perspective on current trends in the Southeastern New England markets. Clients choose to work with me for my years of experience, ethics and commitment to service. My business is 90% referral and 10% new business which I proudly believe speaks volumes about my client’s satisfaction.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:bob@necompass.com">bob@necompass.com</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Bob Nasiatka</h3>
+					<p class="agentParagraph agentElement">Being a realtor for eleven years has allowed me to pair my knowledge with my passion. I have bought and sold my own investment properties since 1997, eventually expanding that endeavor into managing other client’s properties as well. The culmination of that hard work was the establishment of my property management company, R&R Realty, in 2005.<br/><br/>Complementing my years of experience, I work hard to stay on real estate’s cutting edge by using the latest industry technology. This allows me to equip both my buyers and sellers with the most up to date information. As a life-long Rhode Island resident, I also have a unique perspective on current trends in the Southeastern New England markets. Clients choose to work with me for my years of experience, ethics and commitment to service. My business is 90% referral and 10% new business which I proudly believe speaks volumes about my client’s satisfaction.</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:bob@necompass.com">bob@necompass.com</a></h4>
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/rick.jpg" alt="Rick Delmastro"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Rick Delmastro</h3>
-						<p class="agentParagraph agentElement">As a relatively new Real Estate Sales Associate of about 1 year, I have gained invaluable knowledge and experience through my affiliation with August Associates, LLC. My personal reason for becoming involved in the Real Estate industry was my interests in business (have owned my own for almost 30 years), investments, and most importantly my desire to assist you, the client, in the sometimes arduous task of buying or selling a home. As a youth football, baseball and lacrosse coach over the past 14 years, I have developed great lasting relationships with players and parents alike, who have appreciated my genuine help-driven style. My main focus is to protect the interests of the client, and I strive to make their experience as pleasurable as possible. I am proud to be a member of this excellent “team”.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:rick@necompass.com">rick@necompass.com</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Rick Delmastro</h3>
+					<p class="agentParagraph agentElement">As a relatively new Real Estate Sales Associate of about 1 year, I have gained invaluable knowledge and experience through my affiliation with August Associates, LLC. My personal reason for becoming involved in the Real Estate industry was my interests in business (have owned my own for almost 30 years), investments, and most importantly my desire to assist you, the client, in the sometimes arduous task of buying or selling a home. As a youth football, baseball and lacrosse coach over the past 14 years, I have developed great lasting relationships with players and parents alike, who have appreciated my genuine help-driven style. My main focus is to protect the interests of the client, and I strive to make their experience as pleasurable as possible. I am proud to be a member of this excellent “team”.</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:rick@necompass.com">rick@necompass.com</a></h4>
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/kari.jpg" alt="Kari Hernandez"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Kari Hernandez</h3>
-						<p class="agentParagraph agentElement">As a Realtor specializing in residential properties, Kari brings an abundant amount of energy, creativity and dedication to her work. Kari first discovered her passion for real estate while working as a Real Estate Administrator and attending college full time. She studied Business Administration and graduated in 2006 with a Bachelors degree in Business Management from Rhode Island College. Kari later transitioned into the Banking/Financial industry where she gained valuable skills and experience that she uses daily in real estate negotiations, educating and building interpersonal relationships with home buyers and sellers and effectively marketing the team's listings.<br/><br/>Friends and clients describe her as ambitious, detailed oriented yet personable and fun to be around. Kari enjoys artistic projects, exploring local restaurants and likes to spend time with her family during her free time. She is wife of a loving firefighter, mom of three boys and actively participates in her community's baseball little league and is a board member of her neighborhood Parent-Teacher Committee.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:kari@necompass.com">kari@necompass.com</a></h4>
-						<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/KHernandezRealtor/">facebook.com/KHernandezRealtor</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Kari Hernandez</h3>
+					<p class="agentParagraph agentElement">As a Realtor specializing in residential properties, Kari brings an abundant amount of energy, creativity and dedication to her work. Kari first discovered her passion for real estate while working as a Real Estate Administrator and attending college full time. She studied Business Administration and graduated in 2006 with a Bachelors degree in Business Management from Rhode Island College. Kari later transitioned into the Banking/Financial industry where she gained valuable skills and experience that she uses daily in real estate negotiations, educating and building interpersonal relationships with home buyers and sellers and effectively marketing the team's listings.<br/><br/>Friends and clients describe her as ambitious, detailed oriented yet personable and fun to be around. Kari enjoys artistic projects, exploring local restaurants and likes to spend time with her family during her free time. She is wife of a loving firefighter, mom of three boys and actively participates in her community's baseball little league and is a board member of her neighborhood Parent-Teacher Committee.</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:kari@necompass.com">kari@necompass.com</a></h4>
+					<h4 class="agentWebsite agentElement">Facebook: <a href="https://www.facebook.com/KHernandezRealtor/">facebook.com/KHernandezRealtor</a></h4>
 				</div>
 				<div class="agentWrapper">
 					<div class="agentImageWrapper">
 						<img class="agentImage" src="/images/debora.jpg" alt="Debora Rotondo"/>
 					</div>
-					<div class="agentInfo">
-						<h3 class="agentName agentElement">Debora Rotondo</h3>
-						<p class="agentParagraph agentElement">Debora is a licensed Realtor&reg; who is a true relationship builder and knows communication is key to understanding the needs of each client. The exciting pursuit of buying, selling or investing in real estate is a major financial decision and Debora will guide each client every step of the way. Debora is also a floral designer and former proprietor of a floral shop. With her artistic abilities, Debora loves to share her ideas with her clients’ in regard to the aesthetic aspects of their real estate interests.<br/><br/>As a licensee several years back, Debora shifted her focus working as a Real Estate Project Manager for a non-profit, community development corporation. As part of the project management team her work consisted of identifying properties for acquisition, with the broad understanding of funding programs, project budgets and project performance. Her endless experiences bring her back into the field of real estate, taking her ‘A’ Game to AUGUST ASSOCIATES LLC, YOUR VALUED GUIDE IN REAL ESTATE.</p>
-						<h4 class="agentEmail agentElement">Email: <a href="mailto:debora@necompass.com">debora@necompass.com</a></h4>
-					</div>
+					<h3 class="agentName agentElement">Debora Rotondo</h3>
+					<p class="agentParagraph agentElement">Debora is a licensed Realtor&reg; who is a true relationship builder and knows communication is key to understanding the needs of each client. The exciting pursuit of buying, selling or investing in real estate is a major financial decision and Debora will guide each client every step of the way. Debora is also a floral designer and former proprietor of a floral shop. With her artistic abilities, Debora loves to share her ideas with her clients’ in regard to the aesthetic aspects of their real estate interests.<br/><br/>As a licensee several years back, Debora shifted her focus working as a Real Estate Project Manager for a non-profit, community development corporation. As part of the project management team her work consisted of identifying properties for acquisition, with the broad understanding of funding programs, project budgets and project performance. Her endless experiences bring her back into the field of real estate, taking her ‘A’ Game to AUGUST ASSOCIATES LLC, YOUR VALUED GUIDE IN REAL ESTATE.</p>
+					<h4 class="agentEmail agentElement">Email: <a href="mailto:debora@necompass.com">debora@necompass.com</a></h4>
 				</div>
 			</div>
 		</div>
